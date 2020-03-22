0f95adffc570f88d0dfb48ed25a9825520789d6d
nwuensche <Wuensche.Niklas@gmail.com>
2020-03-22 20:35:35 +0100

    Adding an AdView to your Project

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.android.gms.ads.AdView;
 import com.mygdx.game.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
@@ -11,6 +12,7 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		AdView a = new AdView(this);
 		initialize(new MyGdxGame(), config);
 	}
 }
